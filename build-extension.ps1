e10b785c4d1181044eced51b07c29bd9f786791e
Jesse Houwing <jesse.houwing@gmail.com>
2023-01-03 20:57:06 +0100

    Update build-extension.ps1

--- build-extension.ps1
+++ build-extension.ps1
@@ -63,7 +63,7 @@ foreach ($extension in $extensions)
     # Generate vss-extension.json
 
     [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding    
-    $extensionManifest.version = "1.$env:VERSION.3"
+    $extensionManifest.version = "1.$env:VERSION.0"
 
     $extensionManifest | ConvertTo-Json -depth 100 | Out-File "_tmp/vss-extension.json" -Encoding utf8NoBOM
     copy .\vss-extension.$($extension.Id).json _tmp
