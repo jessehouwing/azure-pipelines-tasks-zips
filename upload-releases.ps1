5d304a99c02a46569a8922882021867e840dea1f
Jesse Houwing <jesse.houwing@gmail.com>
2023-03-01 15:16:46 +0100

    add lists in a different way

--- upload-releases.ps1
+++ upload-releases.ps1
@@ -8,7 +8,7 @@ foreach ($release in $existingReleases)
     $knownAssets["$release"] = $releaseDetails.assets
 }
 
-foreach ($taskzip in (dir _sxs/*.zip) + (dir .\_download\*.zip))
+foreach ($taskzip in (@(Get-ChildItem _sxs/*.zip) + @(Get-ChildItem .\_download\*.zip)))
 {
     $taskzip.Name -match "-(?<version>\d+\.\d+\.\d+)\.zip" | Out-Null
     $version = [version]$Matches.version
